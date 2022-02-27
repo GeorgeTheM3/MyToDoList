7dad287588a22e5b1dbaf0d48933d98e332f03da
GeorgeTheM3 <99905582+GeorgeTheM3@users.noreply.github.com>
2022-02-27 21:31:00 +0300

    just start

---
+++ MyToDoList/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  MyToDoList
+//
+//  Created by Георгий Матченко on 27.02.2022.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
